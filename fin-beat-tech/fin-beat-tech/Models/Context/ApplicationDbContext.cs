ef11fef565c12c6390c5871a33f1117195038953
Alexander Ivanov <130238642+Caiman-dev@users.noreply.github.com>
2025-04-03 18:32:51 +0300

    add EntityFramework + models

---
+++ fin-beat-tech/fin-beat-tech/Models/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using fin_beat_tech.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace fin_beat_tech.Models.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<DataItem> DataItems { get; set; }
+    }
+}
---
+++ fin-beat-tech/fin-beat-tech/Models/Entities/DataItem.cs
@@ -0,0 +1,9 @@
+﻿namespace fin_beat_tech.Models.Entities
+{
+    public class DataItem
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Value { get; set; }
+    }
+}
